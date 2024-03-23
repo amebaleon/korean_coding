50646b609268c78e3096c7128f19a0cf6f07c3fd
amebaleon <windowleon@gmail.com>
2024-03-23 09:33:04 +0900

    korean.h ver1

---
+++ korean.h
@@ -0,0 +1,24 @@
+#ifndef KOREAN_H
+#define KOREAN_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define 시작 int main()
+#define 출력(x) std::cout << x;
+#define 끝 return 0;
+#define 반환(x) return x;
+#define 반복(x, n) for(int x = 0; x < n; ++x)
+#define 만약(x) if(x)
+#define 아니면 else if
+#define 생성 int
+#define 문장 std::string
+#define 벡터 std::vector
+#define 넣기 push_back
+#define 빼기 pop_back()
+#define 크기 size()
+#define 첫번째 front()
+#define 마지막 back()
+
+#endif
