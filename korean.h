1335e7d23365ec1173c2921331a33ae062fea79e
amebaleon <108870453+amebaleon@users.noreply.github.com>
2024-03-23 13:48:45 +0900

    Update korean coding
    ver.2
    문제점이나 추가적으로 원하시는 사항 있으시면 windowleon@gmail.com으로 메일 보내주세요

--- korean.h
+++ korean.h
@@ -3,8 +3,11 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
+#include <cmath>
+#include <sstream>
+#include <map>
 
+#define 준비 using namespace std;
 #define 시작 int main()
 #define 출력(x) std::cout << x;
 #define 끝 return 0;
@@ -20,5 +23,22 @@
 #define 크기 size()
 #define 첫번째 front()
 #define 마지막 back()
+#define 제곱 pow
+#define 루트 sqrt
+#define 절댓값 abs
+#define 사전 map
+
+#define 공백구분(x, 문자) do { \
+    stringstream ss(x); \
+    string temp; \
+    벡터<문장> 문자; \
+    while (ss >> temp) { \
+        문자.넣기(temp); \
+    } \
+} while(0)
+#define 정수 int
+#define 소수1 double
+#define 소수2 float
+#define 문자 char
 
 #endif
